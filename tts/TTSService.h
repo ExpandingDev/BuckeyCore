8e3427288340ba4d8107f8fa5e14209b5d8dc6de
ExpandingDev <javscripter@yahoo.com>
2019-12-29 01:41:23 -0500

    Starting standardization of TTS

---
+++ tts/TTSService.h
@@ -0,0 +1,25 @@
+#ifndef TTSSERVICEADAPTEE_H
+#define TTSSERVICEADAPTEE_H
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+#include "Service.h"
+
+class TTSService : public Buckey::Service {
+    public:
+        virtual void speak(std::string words ) = 0;
+        virtual void prepareSpeech(std::string words ) = 0;
+        virtual void selectVoice(std::string voiceName ) = 0;
+        virtual std::vector<std::string> getSpeechHistory(uint16_t index, uint8_t range ) = 0;
+	//virtual std::string getState() = 0;
+	    
+	TTSService(std::string version, std::string name) : Buckey::Service(version, name) { };
+	
+	std::function<void(std::string)> voiceSwitchedCallback;
+	std::function<void(bool)> speechStateChangedCallback;
+	std::function<void(std::string)> speechPreparedCallback;        
+
+};
+#endif /* TTSSERVICEADAPTEE_H */
---
+++ tts/TTSServiceAdapter.h
@@ -0,0 +1,29 @@
+#ifndef TTSSERVICEADAPTER_H
+#define TTSSERVICEADAPTER_H
+
+#include <dbus-cxx.h>
+#include <memory>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include "TTSService.h"
+
+#include "ServiceAdapter.h"
+
+class TTSServiceAdapter : public Buckey::ServiceAdapter {
+    protected:
+        TTSServiceAdapter(TTSService * adaptee, std::string path);
+        
+        static void signalSpeechStateChanged(TTSServiceAdapter * a, bool s);
+        static void signalSpeechPrepared(TTSServiceAdapter * a, std::string s);
+        static void signalVoiceSwitched(TTSServiceAdapter * a, std::string s);
+        
+        DBus::signal<void,std::string>::pointer speechPreparedSignal;
+        DBus::signal<void>::pointer speechStartedSignal;
+        DBus::signal<void>::pointer speechStoppedSignal;
+        DBus::signal<void,std::string>::pointer voiceSwitchedSignal;
+
+    public:
+        static std::shared_ptr<TTSServiceAdapter> create(TTSService * adaptee, std::string path );
+};
+#endif /* TTSSERVICEADAPTER_H */
